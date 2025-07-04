388c73764635de1889f1a71295d9772c9a6a1089
KushagraKatiyar06 <150945125+KushagraKatiyar06@users.noreply.github.com>
2025-07-04 04:12:52 -0400

    cleared

--- test_email.py
+++
@@ -1,8 +0,0 @@
-# test_email.py
-from Email import test_email_system
-
-if __name__ == "__main__":
-    print("=== LessonTrack Email System Test ===")
-    # The test_email_system function will use the management email from environment variables
-    test_email_system()
-    print("Test complete!")
